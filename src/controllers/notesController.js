26c04b695ce5383cb9f664529c5ce2dda97acb16
Artem illushchenko <artemillushchenko30@gmail.com>
2026-04-01 17:18:22 +0300

    fix

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -1,11 +1,13 @@
-import { Note } from '../models/note.js';
 import createHttpError from 'http-errors';
-import { notFoundHandler } from '../middleware/notFoundHandler.js';
-import createError from 'http-errors';
+import { Note } from '../models/note.js';
 
-export const getAllNotes = async (req, res) => {
-  const notes = await Note.find();
-  res.status(200).json(notes);
+export const getAllNotes = async (req, res, next) => {
+  try {
+    const notes = await Note.find();
+    res.status(200).json(notes);
+  } catch (error) {
+    next(error);
+  }
 };
 
 export const getNoteById = async (req, res, next) => {
@@ -14,7 +16,7 @@ export const getNoteById = async (req, res, next) => {
     const note = await Note.findById(noteId);
 
     if (!note) {
-      throw createError(404, `Note with id ${noteId} not found`);
+      throw createHttpError(404, `Note with id ${noteId} not found`);
     }
 
     res.status(200).json(note);
@@ -23,30 +25,43 @@ export const getNoteById = async (req, res, next) => {
   }
 };
 
-export const createNote = async (req, res) => {
-  const note = await Note.create(req.body);
-  res.status(201).json(note);
+export const createNote = async (req, res, next) => {
+  try {
+    const note = await Note.create(req.body);
+    res.status(201).json(note);
+  } catch (error) {
+    next(error);
+  }
 };
 
-export const deleteNote = async (req, res) => {
-  const { noteId } = req.params;
-  const note = await Note.findOneAndDelete({
-    _id: noteId,
-  });
-  if (!note) {
-    throw createHttpError(notFoundHandler);
+export const deleteNote = async (req, res, next) => {
+  try {
+    const { noteId } = req.params;
+    const note = await Note.findOneAndDelete({ _id: noteId });
+
+    if (!note) {
+      throw createHttpError(404, 'Note not found');
+    }
+
+    res.status(200).json(note);
+  } catch (error) {
+    next(error);
   }
-  res.status(200).json(note);
 };
 
-export const updateNote = async (req, res) => {
-  const { noteId } = req.params;
+export const updateNote = async (req, res, next) => {
+  try {
+    const { noteId } = req.params;
+    const note = await Note.findOneAndUpdate({ _id: noteId }, req.body, {
+      returnDocument: 'after',
+    });
 
-  const note = await Note.findOneAndUpdate({ _id: noteId }, req.body, {
-    returnDocument: 'after',
-  });
-  if (!note) {
-    throw createHttpError(notFoundHandler);
+    if (!note) {
+      throw createHttpError(404, 'Note not found');
+    }
+
+    res.status(200).json(note);
+  } catch (error) {
+    next(error);
   }
-  res.status(200).json(note);
 };
--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,16 +1,17 @@
 import { HttpError } from 'http-errors';
 
 export const errorHandler = (err, req, res, next) => {
-  console.error('Error Middleware:', err);
-
   if (err instanceof HttpError) {
     return res.status(err.status).json({
-      message: err.message || err.name,
+      status: err.status,
+      message: err.message,
     });
   }
+
   const isProd = process.env.NODE_ENV === 'production';
 
   res.status(500).json({
+    status: 500,
     message: isProd
       ? 'Something went wrong. Please try again later.'
       : err.message,
