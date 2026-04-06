5913e7378a1e8fe7229c99c9f1369d2e6c769003
Artem illushchenko <artemillushchenko30@gmail.com>
2026-04-06 17:27:41 +0300

    Last fix

--- src/validations/notesValidation.js
+++ src/validations/notesValidation.js
@@ -6,41 +6,36 @@ const objectIdValidator = (value, helpers) => {
   return !isValidObjectId(value) ? helpers.message('Invalid id format') : value;
 };
 
-export const noteIdParamSchema = {
-[Segments.PARAMS]: Joi.object({
-  noteId: Joi.string().custom(objectIdValidator).required(),
-}),
-};
-
 export const getAllNotesSchema = {
   [Segments.QUERY]: Joi.object({
     page: Joi.number().integer().min(1).default(1),
     perPage: Joi.number().integer().min(5).max(20).default(10),
-    tag: Joi.string().valid(...TAGS).optional(),
-    search: Joi.string().allow(''),
+    tag: Joi.string().valid(...TAGS).optional().allow(''),
+    search: Joi.string().allow('').optional().trim(),
   }),
 };
 
 export const noteIdSchema = {
   [Segments.PARAMS]: Joi.object({
-    noteId: Joi.string().custom(objectIdValidator).required()
+    noteId: Joi.string().custom(objectIdValidator).required(),
   }),
 };
 
 export const createNoteSchema = {
   [Segments.BODY]: Joi.object({
-    title: Joi.string().min(1).required(),
-    content: Joi.string().allow('').optional(),
-    tag: Joi.string().valid(...TAGS).optional()
+    title: Joi.string().min(1).required().trim(),
+    content: Joi.string().allow('').optional().trim(),
+    tag: Joi.string().valid(...TAGS).optional(),
   }),
 };
+
 export const updateNoteSchema = {
   [Segments.PARAMS]: Joi.object({
     noteId: Joi.string().custom(objectIdValidator).required(),
   }),
-[Segments.BODY]: Joi.object({
-title: Joi.string().min(1).optional(),
-content: Joi.string().allow('').optional(),
-tag: Joi.string().valid(...TAGS).optional(),
-}).min(1)
+  [Segments.BODY]: Joi.object({
+    title: Joi.string().min(1).optional().trim(),
+    content: Joi.string().allow('').optional().trim(),
+    tag: Joi.string().valid(...TAGS).optional(),
+  }).min(1),
 };
