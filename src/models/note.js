7a9bedda033b52471790586a8810d9048d99b3c3
Artem illushchenko <artemillushchenko30@gmail.com>
2026-04-06 16:58:20 +0300

    ДЗ 3

--- src/models/note.js
+++ src/models/note.js
@@ -1,5 +1,6 @@
 import { Schema } from 'mongoose';
 import { model } from 'mongoose';
+import { TAGS } from '../constants/tags.js';
 const noteSchema = new Schema(
   {
     title: {
@@ -14,18 +15,7 @@ const noteSchema = new Schema(
     },
     tag: {
       type: String,
-      enum: [
-        'Work',
-        'Personal',
-        'Meeting',
-        'Shopping',
-        'Ideas',
-        'Travel',
-        'Finance',
-        'Health',
-        'Important',
-        'Todo',
-      ],
+      enum: TAGS,
       default: 'Todo',
     },
   },
@@ -33,4 +23,5 @@ const noteSchema = new Schema(
     timestamps: true,
   },
 );
+noteSchema.index({ tag: TAGS });
 export const Note = model('Note', noteSchema);
